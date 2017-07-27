4738a95c37c2070645b2d87f904d097a93cf5b5f
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-27 11:37:21 -0500

    Event javabean created

---
+++ src/com/theironyard/Event.java
@@ -0,0 +1,27 @@
+package com.theironyard;
+
+public class Event {
+    // FIELDS
+    private String name;
+    private String action;
+
+    // CONSTRUCTORS
+
+    // METHODS
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
---
+++ src/com/theironyard/EventLog.java
@@ -0,0 +1,7 @@
+package com.theironyard;
+
+/**
+ * Created by Joe on 7/27/17.
+ */
+public class EventLog {
+}
